ed4b5c76b1300189846a9431caa14334af94ffc7
Nicholas <nicholas@flightlogger.net>
2021-09-27 16:04:25 +0200

    add comment

--- src/click-a-button/hooks/useClickAButton.test.tsx
+++ src/click-a-button/hooks/useClickAButton.test.tsx
@@ -1,6 +1,7 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import { UseClickAButton } from "./index";
 
+// refer to https://kentcdodds.com/blog/how-to-test-custom-react-hooks
 describe("component", () => {
   test("initial of zero", () => {
     const { result } = renderHook(() => UseClickAButton());
