fb1d9724f15c5e11961d22be8e1c605bcf467408
Nicholas <nicholas@flightlogger.net>
2021-09-27 15:57:53 +0200

    better setup of tests

--- src/click-a-button/clickAButton.test.tsx
+++ src/click-a-button/clickAButton.test.tsx
@@ -1,25 +1,24 @@
-import * as React from 'react';
-import {act} from "react-dom/test-utils";
-import {render} from "@testing-library/react";
-import {ClickAButton} from "./index";
-import {UseClickAButton} from './hooks'
-const hooks = { UseClickAButton }
-jest.mock('./hooks')
+import * as React from "react";
+import { act } from "react-dom/test-utils";
+import { render } from "@testing-library/react";
+import { ClickAButton } from "./index";
+import { UseClickAButton } from "./hooks";
+const hooks = { UseClickAButton };
+jest.mock("./hooks");
 
-describe('ClickAButton', () => {
-    it('should render with mocked value', () => {
-        const STATE_SPY = jest.spyOn(hooks, 'UseClickAButton');
-        const CLICK_HANDLER = jest.fn();
-        STATE_SPY.mockReturnValue({
-            handleClick: CLICK_HANDLER,
-            total: 5,
-        });
-        const component  = render(<ClickAButton />);
-        expect(component.container).toHaveTextContent('5');
-        act(() => {
-            component.getByText('5').click();
-        });
-        expect(CLICK_HANDLER).toHaveBeenCalled();
-    })
-})
-
+describe("ClickAButton", () => {
+  test("should render with mocked value", () => {
+    const STATE_SPY = jest.spyOn(hooks, "UseClickAButton");
+    const CLICK_HANDLER = jest.fn();
+    STATE_SPY.mockReturnValue({
+      handleClick: CLICK_HANDLER,
+      total: 5,
+    });
+    const component = render(<ClickAButton />);
+    expect(component.container).toHaveTextContent("5");
+    act(() => {
+      component.getByText("5").click();
+    });
+    expect(CLICK_HANDLER).toHaveBeenCalled();
+  });
+});
--- src/click-a-button/hooks/useClickAButton.test.tsx
+++ src/click-a-button/hooks/useClickAButton.test.tsx
@@ -1,27 +1,33 @@
 import * as React from "react";
-import {UseClickAButton} from "./index";
-import {render, act} from '@testing-library/react'
-import {IUseClickAButton} from "./useClickAButton";
-
+import { UseClickAButton } from "./index";
+import { render, act } from "@testing-library/react";
+import { IUseClickAButton } from "./useClickAButton";
 
 function setup() {
-    const returnVal = {} as IUseClickAButton
-    function TestComponent() {
-        Object.assign(returnVal, UseClickAButton())
-        return null
-    }
-    render(<TestComponent />)
-    return returnVal
+  const returnVal = {} as IUseClickAButton;
+  function TestComponent() {
+    Object.assign(returnVal, UseClickAButton());
+    return null;
+  }
+  render(<TestComponent />);
+  return returnVal;
 }
 
-test('test component', () =>{
-    const clickAButton = setup()
+describe("component", () => {
+  let clickAButton: IUseClickAButton;
+  beforeEach(() => {
+    clickAButton = setup();
+  });
 
-    expect(clickAButton.total).toBe(0)
+  test("initial of zero", () => {
+    expect(clickAButton.total).toBe(0);
+  });
 
+  test("1 press increments to 1", () => {
     act(() => {
-        clickAButton.handleClick()
-    })
+      clickAButton.handleClick();
+    });
 
-    expect(clickAButton.total).toBe(1)
-})
+    expect(clickAButton.total).toBe(1);
+  });
+});
